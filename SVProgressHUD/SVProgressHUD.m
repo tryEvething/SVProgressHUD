5109121c46e9afb5d78a8efdb14f33fbd09892c5
BIMiracle <18730979+tryEvething@users.noreply.github.com>
2019-12-17 17:51:35 +0800

    fix iOS11.4.1 没有宽高

--- SVProgressHUD/SVProgressHUD.m
+++ SVProgressHUD/SVProgressHUD.m
@@ -68,11 +68,11 @@ static const CGFloat SVProgressHUDLabelSpacing = 8.0f;
     static dispatch_once_t once;
     
     static SVProgressHUD *sharedView;
-#if !defined(SV_APP_EXTENSIONS)
-    dispatch_once(&once, ^{ sharedView = [[self alloc] initWithFrame:[[[UIApplication sharedApplication] delegate] window].bounds]; });
-#else
+//#if !defined(SV_APP_EXTENSIONS)
+//    dispatch_once(&once, ^{ sharedView = [[self alloc] initWithFrame:[[[UIApplication sharedApplication] delegate] window].bounds]; });
+//#else
     dispatch_once(&once, ^{ sharedView = [[self alloc] initWithFrame:[[UIScreen mainScreen] bounds]]; });
-#endif
+//#endif
     return sharedView;
 }
 
